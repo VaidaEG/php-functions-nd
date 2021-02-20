606fbc4032f808137852211fb9d457936a5bc7cc
VaidaEG <grausliene.vaida@gmail.com>
2021-02-20 22:11:51 +0200

    Functions 3-4 tasks

--- index.php
+++ index.php
@@ -20,3 +20,36 @@ echo text2($text1, $randomTag);
 ?>
 <h1>---------------------------------3.---------------------------------</h1>
 <p>Generuokite atsitiktinį stringą, pasinaudodami kodu md5(time()). Visus skaitmenis stringe įdėkite į h1 tagą. Jegu iš eilės eina keli skaitmenys, juos į tagą reikią dėti kartu (h1 atsidaro prieš pirmą ir užsidaro po paskutinio) Keitimui naudokite pirmo uždavinio funkciją ir preg_replace_callback()</p>
+<?php
+function textToH1($text10) {
+    return "<h1>$text10</h1>";
+};
+echo textToH1('Labas!');
+
+$randomString = md5(time());
+echo $randomString;
+function numbersToH1($text) {
+    $stringNumbers = preg_replace_callback('/[0-9]+/', function($matches) {return textToH1($matches[0]);}, $text);
+    return $stringNumbers;
+}
+echo numbersToH1($randomString);
+?>
+<h1>---------------------------------4.---------------------------------</h1>
+<p>Parašykite funkciją, kuri skaičiuotų, iš kiek sveikų skaičių jos argumentas dalijasi be liekanos (išskyrus vienetą ir patį save) Argumentą užrašykite taip, kad būtų galima įvesti tik sveiką skaičių;
+</p>
+<?php
+function countDividers(int $number) {
+    $dividers = 0;
+    // if (!is_int($number)) {
+    //     echo 'Enter only integer!';
+    // } else {
+        for ($i = 2; $i < $number; $i++) {
+            if ($number % $i === 0) {
+                $dividers++;
+            }
+        }
+        return $dividers;
+    // }
+}
+echo countDividers(110);
+?>
